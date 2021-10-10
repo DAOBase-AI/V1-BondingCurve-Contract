83f95bbb6c22100d31dae49e3d0f9ce37e721e07
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-11 02:29:20 +0800

    Update CurveFactory.sol
    变量名修改、注释翻译及更新
    CurveFactory
    Creator 改为 platform
    platformProfitRate 改为 platformRate
    setCreator 改为 setPlatform
    _creator 改为 _platform
    setPlatformProfitRate 改为 setPlatformRate
    _platformProfitRate 改为 _platformRate
    _bussinessCreator 改为 _creator
    _bussinessRate 改为 _creatorRate
    算法修改
    CurveFactory
    https://github.com/Mialalala1/bonding-curve-contract/blob/main/CurveFactory.sol#L45
    _creatorRate <= 19 改为 _creatorRate <= 50 - platformRate, 这里的总限制是针对creator和platform佣金总和的限制，顺便把20改成50把适用范围更大一些（或者可不可以也设成可修改的变量？）

--- CurveFactory.sol
+++ CurveFactory.sol
@@ -12,8 +12,8 @@ import "./Curve.sol";
 contract CurveFactory is Ownable {
     using EnumerableSet for EnumerableSet.AddressSet;
     
-    address payable public creator;
-    uint256 public platformProfitRate = 1;
+    address payable public platform;            //platform commision account
+    uint256 public platformRate = 1;      //% of total minting cost as platform commission
     address[] public curves;
     mapping(address => address) public curveOwnerMap;
     mapping(address => EnumerableSet.AddressSet) private userCurves;
@@ -21,45 +21,45 @@ contract CurveFactory is Ownable {
     constructor() {
     }
     
-    // 设置平台手续费收取账号
-    function setCreator(address payable _creator) public onlyOwner {
-        creator = _creator;
+    // set up the platform commission account
+    function setPlatform(address payable _platform) public onlyOwner {
+        platform = _platform;
     }
 
-    // 设置平台手续费比例，只能由owner操作
-    // _platformProfitRate: 百分比，1表示1%
-    function setPlatformProfitRate(uint256 _platformProfitRate) public onlyOwner {
-        platformProfitRate = _platformProfitRate;
+    // set the platform commission rate, only operable by contract owner, _platformRate is in pph
+    function setPlatformRate(uint256 _platformRate) public onlyOwner {
+        platformRate = _platformRate;
     }
     
-    // 创建curve
-    // _bussinessCreator: B端用户设置的手续费收取账号
-    // _bussinessRate: B端用户收取的手续费比例
-    // _virtualBalance: 虚拟流动性数量，譬如设置为10，表示铸造第0个NFT时的费用实际是按照第11个NFT的费用进行计价
-    // _erc20: 表示用户铸造NFT时需要支付哪种ERC20，如果此值为0x000...000, 表示用户需要通过支付ETH来铸造NFT
-    // _initMintPrice: 铸造NFT的价格系数，即f(x)=m*x^n+m中的m的值
-    // _n,_d: _n/_d即f(x)=m*x^n+m中n的值
-    function createCurve(address payable _bussinessCreator, uint256 _bussinessRate,
+    /**@dev build curve
+    *_creator: creator set up commission account
+    *_creatorRate: creator's commission rate
+    *_virtualBalance: Displacement of bonding curve for setting a reasonable initial price
+    *_erc20: contribution token, user need to pay ETH if it's zero address
+    *_initMintPrice: slope of bonding curve
+    *_n,_d: _n/_d = N of f(X) = m*(X^N) + v
+    */
+    function createCurve(address payable _creator, uint256 _creatorRate,       
                          uint256 _virtualBalance, address _erc20, uint256 _initMintPrice,
                          uint256 _n, uint256 _d) public {
-        require(_bussinessRate <= 19, "C: bussinessRate is too high.");   
-        address curve = address(new Curve(creator, platformProfitRate, _bussinessCreator, _bussinessRate, _virtualBalance, _erc20, _initMintPrice, _n, _d));
+        require(_creatorRate <= 50 - platformRate, "C: creatorRate is too high.");   
+        address curve = address(new Curve(platform, platformRate, _creator, _creatorRate, _virtualBalance, _erc20, _initMintPrice, _n, _d));
         userCurves[msg.sender].add(curve);
         curves.push(curve);
         curveOwnerMap[curve] = msg.sender;
     }
     
-    // 获取已经创建好的curve总数
+    // total number of curves created via factory account
     function getCurveTotalNumber() public view returns(uint256) {
         return curves.length;
     }
     
-    // 获取某个用户已经创建好的curve总数
+    // total number of curves created by a user
     function getUserCurveNumber(address _userAddr) public view returns(uint256) {
         return userCurves[_userAddr].length();
     }
     
-    // 通过用户地址和序号获取curve地址
+    // Get the curve address by user address and index
     function getUserCurve(address _userAddr, uint256 _index) public view returns(address) {
         return userCurves[_userAddr].at(_index);
     }
