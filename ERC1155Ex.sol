4e8138ebe2da3f56a022e364ce7535ab236bd7f5
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-11 02:31:10 +0800

    Update ERC1155Ex.sol
    疑问
    ERC1155ex
    如果要通过合约获取一个用户的所有token余额，应该是先call getUserTokenIdNumber，然后根据每个token id再去查余额对吧？

--- ERC1155Ex.sol
+++ ERC1155Ex.sol
@@ -4,22 +4,22 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-// 增强型ERC1155，记录了某用户拥有的所有token ID
+// extended ERC1155 which records all tokens IDs owned by a user
 contract ERC1155Ex is ERC1155 {
     using EnumerableSet for EnumerableSet.UintSet;
     
     address public curve;
-    uint256 public totalSupply;         // 总流通量
+    uint256 public totalSupply;         // total supply of erc1155 tokens
     uint256 public tokenId;
-    mapping(address => EnumerableSet.UintSet) private userTokenIds;   // 记录用户拥有的tokenId
+    mapping(address => EnumerableSet.UintSet) private userTokenIds;   // record all users' token IDs
     
     constructor () ERC1155("") {
         curve = msg.sender;
     }
     
-    // 给_to铸造ERC1155，数量_balance
+    // mint a batch of erc1155 tokens to address(_to).
     function mint(address _to, uint256 _balance) public returns(uint256) {
-        require(msg.sender == curve, "NFT1155: Minter is not the curve");
+        require(msg.sender == curve, "NFT1155: Minter is not the CURVE");   
         _mint(_to, tokenId, _balance, "");
         tokenId += 1;
         totalSupply += _balance;
@@ -27,9 +27,12 @@ contract ERC1155Ex is ERC1155 {
         return tokenId;
     }
     
-    // 销毁_to拥有的ERC1155, ID为_tokenId，销毁数量为_balance
+    /**@dev Destroy erc1155 tokens owned by address(_to)
+    *ID: token Id
+    *_balance: number of erc1155 tokens destroyed
+    */
     function burn(address _to, uint256 _tokenId, uint256 _balance) public {
-        require(msg.sender == curve, "NFT1155: Minter is not the curve");
+        require(msg.sender == curve, "NFT1155: Minter is not the CURVE");
         _burn(_to, _tokenId, _balance);
         totalSupply -= _balance;
         if (balanceOf(_to, _tokenId) == 0) {
@@ -37,12 +40,12 @@ contract ERC1155Ex is ERC1155 {
         }
     }
 
-    // 获取某个用户拥有的ERC1155 token ID总数
+    // Get the total number of ERC1155 token IDs owned by a user
     function getUserTokenIdNumber(address _userAddr) public view returns(uint256) {
         return userTokenIds[_userAddr].length();
     }
     
-    // 通过用户地址和序号获取tokenId
+    // Get token IDs by user address and index number
     function getUserTokenId(address _userAddr, uint256 _index) public view returns(uint256) {
         return userTokenIds[_userAddr].at(_index);
     }
