6df4d632d774fec3e0a9fc6753979b04e4b3a0b1
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-19 00:43:18 +0800

    add English comment and slight modification

--- Curve.sol
+++ Curve.sol
@@ -8,101 +8,111 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./ERC1155Ex.sol";
 import "./math-utils/AnalyticMath.sol";
 
-// Curve类，通过ERC20铸造NFT
+/**
+* @dev thePASS Bonding Curve - minting NFT through erc20 or eth
+* PASS is orgnization's erc1155 token on curve 
+* erc20 or eth is contribution token on curve 
+* Users can mint PASS via deposting erc20 contribution token into curve
+* thePASS Bonding Curve Formula: f(X) = m*(x^N)+v
+* f(x) = PASS Price when total supply of PASS is x
+* m, slope of bonding curve
+* x = total supply of PASS 
+* N = n/d
+* v = virtual balance, Displacement 
+of bonding curve
+*/
 contract Curve {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public erc20;          // 铸造NFT需要消耗的ERC20
-    uint256 public initMintPrice; // 初始铸造价格
-    uint256 public n;             // 幂分子
-    uint256 public d;             // 幂分母
-    uint256 public intPower;         // 幂为整数时的值
-    uint256 public virtualBalance;  // 虚拟流动性数值
+    IERC20 public erc20;             // contribution token on bonding curve
+    uint256 public m;                // slope of bonding curve
+    uint256 public n;                // numerator of exponent in curve power function
+    uint256 public d;                // denominator of exponent in curve power function
+    uint256 public intPower;         // when n/d is integer
+    uint256 public virtualBalance;   // vitual balance for setting a reasonable initial price
+    // cache of power function calculation result when exponent is decimal，n => cost
+    mapping(uint256 => uint256) public decPowerCache;   
+    uint256 public reserve;          // reserve of contribution tokens stored in bonding curve AMM pool
     
-    mapping(uint256 => uint256) public decPowerCache;   // 小数幂计算结果缓存，n => cost
-    uint256 public reserve;   // 本合约当前拥有的ERC20数量
+    address payable public platform; // thePass platform's commission account
+    uint256 public platformRate;     // thePass platform's commission rate in pph  
     
-    address payable public platform;   // 平台费收取账号
-    uint256 public platformRate;              // 平台费收取比例
-    
-    address payable public creator;  // B端手续费收取账号
-    uint256 public creatorRate;             // B端手续费收取比例
-
-    ERC1155Ex public thePASS;                // ERC1155Ex合约
-    
-    IAnalyticMath public analyticMath = IAnalyticMath(0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8);  // 数学方法，可计算幂函数
+    address payable public creator;  // creator's commission account
+    uint256 public creatorRate;      // creator's commission rate in pph
+    // thePASS extened ERC1155 contract
+    ERC1155Ex public thePASS;        
+    // Mathmatical method for calculating power function
+    IAnalyticMath public analyticMath = IAnalyticMath(0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8); 
 
     event Minted(uint256 indexed tokenId, uint256 indexed cost, uint256 indexed reserveAfterMint, uint256 balance);
     event Burned(uint256 indexed tokenId, uint256 indexed returnAmount, uint256 indexed reserveAfterBurn, uint256 balance);
     event BatchBurned(uint256[] tokenIds, uint256[] balances, uint256 indexed returnAmount, uint256 indexed reserveAfterBurn);
 
-    /*
-    构造函数
-    _virtualBalance: 虚拟流动性，即f(x)=m*x^n+v中的v的值
-    _erc20: 表示用户铸造NFT时需要支付哪种ERC20，如果此值为0x000...000, 表示用户需要通过支付ETH来铸造NFT
-    _initMintPrice: 铸造NFT的价格系数，即f(x)=m*x^n+v中的m的值
-    _n,_d: _n/_d即f(x)=m*x^n+v中n的值
+    /**
+    * @dev constrcutor of bonding curve with formular: f(x)=m*(x^N)+v
+    * _initMintPrice: f(1) = m + v, the first PASS minting price
+    * _m: m, slope of curve
+    * _virtualBalance: v = f(1) - _m, virtual balance, displacement of curve
+    * _n,_d: _n/_d = N, exponent of the curve
+    * _erc20: contribution token(erc20) for miniting PASS on bonding curve, send 0x000...000 to appoint it as eth instead of erc20 token
+    * reserve: reserve of contribution tokens stored in bonding curve AMM pool, start with 0
     */
-    constructor (address _erc20, uint256 _virtualBalance, uint256 _initMintPrice,
+    constructor (address _erc20, uint256 _m, uint256 _initMintPrice,
                  uint256 _n, uint256 _d, string memory _baseUri) {
         erc20 = IERC20(_erc20);
-        initMintPrice = _initMintPrice;
-        
+        m = _m;
         n = _n;
-        d = _d;
+        
+
         if (_n.div(_d).mul(_d) == _n) {
             intPower = _n.div(_d);
         }
-        virtualBalance = _virtualBalance;
-        
+        else { d = _d; }
+
+        v = _initMintPrice - _m;
         reserve = 0;
-        thePASS = new ERC1155Ex(_baseUri); 
+        thePASS = new ERC1155Ex(_baseUri);  // setting the NFT PASS contract as orgnization token with initial baseURI
     }
     
-    /* 设置手续费相关参数
-    _platformCreator: 平台手续费收取账号
-    _platformRate: 平台手续费比例
-    _bussinessCreator: B端用户设置的手续费收取账号
-    _bussinessRate: B端用户收取的手续费比例
-    */
+    // @creator commission account and rate initilization
     function setFeeParameters(address payable _platform, uint256 _platformRate, 
                               address payable _creator, uint256 _createRate) public {
-        require(platform == address(0) && creator == address(0), "C: fee inited.");
+        require(platform == address(0) && creator == address(0), "Curve: commission account and rate cannot be modified.");
         platform = _platform;
         platformRate = _platformRate;
         creator = _creator;
         creatorRate = _createRate;
     }
 
-    /*
-    通过ERC20铸造ERC1155，每次铸造都会生成一个独立ID的ERC1155
-    _balance: 铸造数量
-    _amount: 用户设定的最大可转让金额，当实际所需金额小于此值时，合约只会转实际的金额
-    _maxPriceFistNFT：本次铸造的第一个NFT的最大允许金额，防止front-running
-    返回值: token ID
+    /**
+    * @dev each PASS minting transaction by depositing contribution erc20 tokens will create a new erc1155 token id sequentially
+    * _balance: number of PASSes user want to mint
+    * _amount: the maximum deposited amount set by the user
+    * _maxPriceFistNFT: the maximum price for the first mintable PASS to prevent front-running with least gas consumption
+    * return: token id
     */
     function mint(uint256 _balance, uint256 _amount, uint256 _maxPriceFirstPASS) public returns (uint256) {  
-        require(address(erc20) != address(0), "C: erc20 is null.");
-        uint256 firstPrice = caculateCurrentCostToMint(1);
-        require(_maxPriceFirstPASS >= firstPrice, "C: price is too high.");
+        require(address(erc20) != address(0), "Curve: erc20 address is null.");
+        uint256 firstPrice = getCurrentCostToMint(1);
+        require(_maxPriceFirstPASS >= firstPrice, "Curve: price exceeds slippage tolerance.");
 
-        return mintNFT(_balance, _amount, false);
+        return _mint(_balance, _amount, false);
     }
-
+    // for small amount of PASS minting, it will be unceessary to check the maximum price for the fist mintable PASS
     function mint(uint256 _balance, uint256 _amount) public returns (uint256) {  
-        require(address(erc20) != address(0), "C: erc20 is null.");
+        require(address(erc20) != address(0), "Curve: erc20 address is null.");
         
-        return mintNFT(_balance, _amount, false);
+        return _mint(_balance, _amount, false);
     }
     
-    /*
-    用户销毁ERC1155, 获得相应的ERC20
-    _tokenId: 待销毁的ERC1155的ID
-    _balance: 销毁数量
+    /**
+    * @dev user burn PASS/PASSes to receive corresponding contribution tokens
+    * _tokenId: the token id of PASS/PASSes to burn
+    * _balance: the number of PASS/PASSes to burn
     */
     function burn(uint256 _tokenId, uint256 _balance) public {
-        require(address(erc20) != address(0), "C: erc20 is null.");
+        require(address(erc20) != address(0), "Curve: erc20 address is null.");
         uint256 burnReturn = getCurrentReturnToBurn(_balance);
         
         // checks if allowed to burn
@@ -113,10 +123,10 @@ contract Curve {
 
         emit Burned(_tokenId, burnReturn, reserve, _balance);
     }
-    
+    // allow user to burn batches of PASSes with a set of token id
     function burnBatch(uint256[] memory _tokenIds, uint256[] memory _balances) public {
-        require(address(erc20) != address(0), "C: erc20 is null.");
-        require(_tokenIds.length == _balances.length, "C: _tokenIds and _balances length mismatch");
+        require(address(erc20) != address(0), "Curve: erc20 address is null.");
+        require(_tokenIds.length == _balances.length, "Curve: _tokenIds and _balances length mismatch");
         uint256 totalBalance;
         for(uint256 i = 0; i < _balances.length; i++) {
             totalBalance += _balances[i];
@@ -133,34 +143,33 @@ contract Curve {
         emit BatchBurned(_tokenIds, _balances, burnReturn, reserve);
     }
     
-    
-    /*
-    通过ETH铸造ERC1155，每次铸造都会生成一个独立ID的ERC1155
-    _balance: 铸造数量
-    _maxPriceFistNFT：本次铸造的第一个NFT的最大允许金额，防止front-running
-    返回值: token ID
+    /**
+    * @dev each PASS minting transaction by depositing contribution ETHs will create a new erc1155 token id sequentially
+    * _balance: number of PASSes user want to mint
+    * _maxPriceFirstPASS: the maximum price for the first mintable PASS to prevent front-running with least gas consumption
+    * return: token ID
     */
     function mintEth(uint256 _balance, uint256 _maxPriceFirstPASS) payable public returns (uint256) {    
-        require(address(erc20) == address(0), "C: erc20 is NOT null.");    
-        uint256 firstPrice = caculateCurrentCostToMint(1);
-        require(_maxPriceFirstPASS >= firstPrice, "C: price is too high.");
+        require(address(erc20) == address(0), "Curve: erc20 address is NOT null.");    
+        uint256 firstPrice = getCurrentCostToMint(1);
+        require(_maxPriceFirstPASS >= firstPrice, "Curve: price is too high.");
 
-        return mintNFT(_balance, msg.value, true);
+        return _mint(_balance, msg.value, true);
     }
 
     function mintEth(uint256 _balance) payable public returns (uint256) {    
-        require(address(erc20) == address(0), "C: erc20 is NOT null.");    
+        require(address(erc20) == address(0), "Curve: erc20 address is NOT null.");    
         
-        return mintNFT(_balance, msg.value, true);
+        return _mint(_balance, msg.value, true);
     }
 
-    /*
-    用户销毁ERC1155，获得相应的ETH
-    _tokenId: 待销毁的ERC1155的ID
-    _balance: 销毁数量
+    /**
+    * @dev user burn PASS/PASSes to receive corresponding contribution ETHs
+    * _tokenId: the token id of PASS/PASSes to burn
+    * _balance: the number of PASS/PASSes to burn
     */
     function burnEth(uint256 _tokenId, uint256 _balance) public {
-        require(address(erc20) == address(0), "C: erc20 is NOT null.");
+        require(address(erc20) == address(0), "Curve: erc20 address is NOT null.");
         uint256 burnReturn = getCurrentReturnToBurn(_balance);
         
         thePASS.burn(msg.sender, _tokenId, _balance);
@@ -171,12 +180,11 @@ contract Curve {
         emit Burned(_tokenId, burnReturn, reserve, _balance);
     }
     
-    /*
-    铸造ERC1155的底层函数
-    */
-    function mintNFT(uint256 _balance, uint256 _amount, bool bETH) private returns (uint256) {
-        uint256 mintCost = caculateCurrentCostToMint(_balance);
-        require(_amount >= mintCost, "C: Not enough token sent");
+    // internal function to mint PASS
+    function _mint(uint256 _balance, uint256 _amount, bool bETH) private returns (uint256) {
+        uint256 mintCost = getCurrentCostToMint(_balance);
+        require(_amount >= mintCost, "Curve: not enough tokens sent.");
+        // For eth, any excess amount will be returned. For erc20, only required amount will be transferred
         if (bETH) {
             if (_amount.sub(mintCost) > 0)
                 payable(msg.sender).transfer(_amount.sub(mintCost));
@@ -204,29 +212,32 @@ contract Curve {
         return tokenId; // returns tokenId in case its useful to check it
     }
 
-    /*
-    获取一次铸造_balance个NFT的费用，公式f(x)=m*x^n+v
-    _balance: 铸造NFT的数量
+    /**
+    * @dev calculate the cost of minting _balance PASSes in a transaction
+    * _balance: number of PASS/PASSes to mint
     */
-    function caculateCurrentCostToMint(uint256 _balance) public returns (uint256) {
-        uint256 curStartX = getCurrentSupply() + 1;
+    function getCurrentCostToMint(uint256 _balance) public view returns (uint256) {
+        uint256 curStartX = getCurrentSupply() + 1;     // fist PASS to mint
         uint256 totalCost;
         if (intPower > 0) {
             if (intPower <= 10) {
+                // for intergal n from 1 ~ 10, using Bernoulli's formula to make calculation gas consumption manageable
                 uint256 intervalSum = caculateIntervalSum(intPower, curStartX, curStartX + _balance - 1);
-                totalCost = initMintPrice.mul(intervalSum).add(virtualBalance.mul(_balance));
+                totalCost = m.mul(intervalSum).add(virtualBalance.mul(_balance));
             } else {
                 for (uint256 i = curStartX; i < curStartX + _balance; i++) {
-                    totalCost = totalCost.add(initMintPrice.mul(i**intPower).add(virtualBalance));
+                    totalCost = totalCost.add(m.mul(i**intPower).add(virtualBalance));
                 }
             }
         } else {
+            // calculation method for decimal n
             for (uint256 i = curStartX; i < curStartX + _balance; i++) {
                 if (decPowerCache[i] == 0) {
                     (uint256 p, uint256 q) = analyticMath.pow(i, 1, n, d);
-                    uint256 cost = virtualBalance.add(initMintPrice.mul(p).div(q));
+                    uint256 cost = virtualBalance.add(m.mul(p).div(q));
                     totalCost = totalCost.add(cost);
-                    decPowerCache[i] = cost;
+                    // cache each caculated f(x) to save next-time calculation gas consumption
+                    decPowerCache[i] = cost;        
                 } else {
                     totalCost = totalCost.add(decPowerCache[i]);
                 }
@@ -235,44 +246,23 @@ contract Curve {
         return totalCost;
     }
 
-    function getCurrentCostToMint(uint256 _balance) public view returns (uint256) {
-        uint256 curStartX = getCurrentSupply() + 1;
-        uint256 totalCost;
-        if (intPower > 0) {
-            if (intPower <= 10) {
-                uint256 intervalSum = caculateIntervalSum(intPower, curStartX, curStartX + _balance - 1);
-                totalCost = initMintPrice.mul(intervalSum).add(virtualBalance.mul(_balance));
-            } else {
-                for (uint256 i = curStartX; i < curStartX + _balance; i++) {
-                    totalCost = totalCost.add(initMintPrice.mul(i**intPower).add(virtualBalance));
-                }
-            }
-        } else {
-            for (uint256 i = curStartX; i < curStartX + _balance; i++) {
-                (uint256 p, uint256 q) = analyticMath.pow(i, 1, n, d);
-                uint256 cost = virtualBalance.add(initMintPrice.mul(p).div(q));
-                totalCost = totalCost.add(cost);
-            }
-        }
-        return totalCost;
-    }
-
-    /*
-    获取一次销毁_balance个NFT的费用
-    _balance: 销毁NFT的数量
+    /**
+    * @dev calculate the return of burning _balance PASSes in a transaction
+    * _balance: number of PASS/PASSes to be burned
     */
     function getCurrentReturnToBurn(uint256 _balance) public view returns (uint256) {
         uint256 curEndX = getCurrentSupply();
-        _balance = _balance > curEndX ? curEndX : _balance;
+        // required burning balance cannot exceed current supply
+        _balance = _balance > curEndX ? curEndX : _balance;           
 
         uint256 totalReturn;
         if (intPower > 0) {
             if (intPower <= 10) {
                 uint256 intervalSum = caculateIntervalSum(intPower, curEndX - _balance + 1, curEndX);
-                totalReturn = initMintPrice.mul(intervalSum).add(virtualBalance.mul(_balance));
+                totalReturn = m.mul(intervalSum).add(virtualBalance.mul(_balance));
             } else {
                 for (uint256 i = curEndX; i > curEndX - _balance; i--) {
-                    totalReturn = totalReturn.add(initMintPrice.mul(i**intPower).add(virtualBalance));
+                    totalReturn = totalReturn.add(m.mul(i**intPower).add(virtualBalance));
                 }
             }
         } else {
@@ -284,12 +274,12 @@ contract Curve {
         return totalReturn;
     }
 
-    // 获取当前存在的NFT数量
+    // get current supply of PASS
     function getCurrentSupply() public view returns (uint256) {
         return thePASS.totalSupply();
     }
 
-    function caculateIntervalSum(uint256 _power, uint256 _startX, uint256 _endX) view public returns(uint256) {
+    function caculateIntervalSum(uint256 _power, uint256 _startX, uint256 _endX) public view returns(uint256) {
         return analyticMath.caculateIntPowerSum(_power, _endX).sub(analyticMath.caculateIntPowerSum(_power, _startX - 1));
     }
 
