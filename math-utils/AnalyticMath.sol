155e880e88151daac423c2cb170d1c35cec6aeb5
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-20 02:11:11 +0800

    Add @dev Bernoulli's formula

--- math-utils/AnalyticMath.sol
+++ math-utils/AnalyticMath.sol
@@ -48,6 +48,7 @@ contract AnalyticMath {
         return (p, q);
     }}
     
+    // @dev Bernoulli's formula
     // 1: n^2/2 + n/2
     // 2: (2*n^3 + 3*n^2 + n)/6   
     // 3: (n^4 + 2*n^3 + n^2)/4                                     
