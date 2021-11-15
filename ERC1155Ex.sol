aadbe1130246d8a84c95e23ab5c3d1961f94c516
The PASS <service@thepass.to>
2021-11-15 18:27:02 +0800

    Remove userTokenIds

--- ERC1155Ex.sol
+++ ERC1155Ex.sol
@@ -4,14 +4,13 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-// extended ERC1155 which records all tokens IDs owned by a user
 contract ERC1155Ex is ERC1155 {
     using EnumerableSet for EnumerableSet.UintSet;
     
     address public curve;
     uint256 public totalSupply;         // total supply of erc1155 tokens
     uint256 public tokenId;
-    mapping(address => EnumerableSet.UintSet) private userTokenIds;   // record all users' token IDs
+//    mapping(address => EnumerableSet.UintSet) private userTokenIds;   // record all users' token IDs
     
     constructor (string memory _baseUri) ERC1155(_baseUri) {
         curve = msg.sender;
@@ -23,7 +22,7 @@ contract ERC1155Ex is ERC1155 {
         tokenId += 1;
         _mint(_to, tokenId, _balance, "");
         totalSupply += _balance;
-        userTokenIds[_to].add(tokenId);
+//        userTokenIds[_to].add(tokenId);
         return tokenId;
     }
     
@@ -36,10 +35,10 @@ contract ERC1155Ex is ERC1155 {
         require(msg.sender == curve, "ERC1155Ex: minter is not the curve");
         _burn(_to, _tokenId, _balance);
         totalSupply -= _balance;
-        if (balanceOf(_to, _tokenId) == 0) {
+/*        if (balanceOf(_to, _tokenId) == 0) {
             userTokenIds[_to].remove(_tokenId);
         }
-    }
+*/    }
     
     // burn a batch of erc1155 tokens
     function burnBatch(address _to, uint256[] memory _tokenIds, uint256[] memory _balances) public {
@@ -49,12 +48,12 @@ contract ERC1155Ex is ERC1155 {
         
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             totalSupply -= _balances[i];
-            if (balanceOf(_to, _tokenIds[i]) == 0) {
+/*            if (balanceOf(_to, _tokenIds[i]) == 0) {
                 userTokenIds[_to].remove(_tokenIds[i]);
-            }    
-        }
+            }   
+*/      }
     }
-
+/*
     // Get the total number of ERC1155 token IDs owned by a user
     function getUserTokenIdNumber(address _userAddr) public view returns(uint256) {
         return userTokenIds[_userAddr].length();
@@ -65,3 +64,4 @@ contract ERC1155Ex is ERC1155 {
         return userTokenIds[_userAddr].at(_index);
     }
 }
+*/
