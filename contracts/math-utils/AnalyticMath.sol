a872b9f3f31c77468bc8f20ea9bf22e8470fd4e1
thepassowner <94793423+thepassowner@users.noreply.github.com>
2021-12-09 10:44:58 +0800

    correct the name of formular in code comment

--- contracts/math-utils/AnalyticMath.sol
+++ contracts/math-utils/AnalyticMath.sol
@@ -48,7 +48,7 @@ contract AnalyticMath {
         return (p, q);
     }}
     
-    // @dev Bernoulli's formula
+    // @dev Faulhaber's formula
     // 1: n^2/2 + n/2
     // 2: (2*n^3 + 3*n^2 + n)/6   
     // 3: (n^4 + 2*n^3 + n^2)/4                                     
